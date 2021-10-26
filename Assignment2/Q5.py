25382d0030d401d49894e2b63d36fa9489ea40f8
panrong <1061743513@qq.com>
2021-10-26 21:08:32 +0800

    add: Assignment 2 Q5

---
+++ Assignment2/Q5.py
@@ -0,0 +1,34 @@
+# Input: string S ; string T
+# Output: the number of subsequences of S equal to T
+
+import numpy as np
+
+
+def LargestSubsequence(S, T):
+    m = len(S)
+    n = len(T)
+    dp = np.zeros((len(S)+1, len(T)+1))
+
+    if m < n:
+        return 0
+
+    for i in range(m+1):
+        dp[i][n] = 1
+    for i in range(m - 1, -1,-1):
+        for j in range(n - 1, -1, -1):
+            # if i == m - 1:
+            #     dp[i][j] = 0
+            # if j == n - 1:
+            #     dp[i][j] = 1
+            if S[i] != T[j]:
+                dp[i][j] = dp[i + 1][j]
+            if S[i] == T[j]:
+                dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+
+    print(dp)
+    return dp[0][0]
+
+
+S = "babgbag"
+T = "bag"
+print(LargestSubsequence(S, T))
